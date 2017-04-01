863a6096f18c9ca2bf30a5889aca401e0802f7e7
ruchi <ruchipatel@Aalaps-Air.home>
2017-03-31 20:25:24 -0400

    changed sql queries

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 
-artists = ['Wolfmother','Kygo','Justin Bieber','DNCE','Sia','Rihanna']
+artists = ['Drake', 'Adele', 'Coldplay', 'Khalid','Future','Calvin Harris', 'Katy Perry','Nicky Jam', 'U2','Wolfmother','Kygo','Justin Bieber','DNCE','Sia','Rihanna']
 
 artists.each do |artist|
   artist_info = RSpotify::Artist.search(artist).first
