4aa41ddda635feaf68b4793ddd0c5f1327e51c82
ruchi <ruchipatel@Aalaps-Air.home>
2017-04-05 15:42:54 -0400

    fixed search

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,11 +2,11 @@ class Song < ApplicationRecord
   validates :name, presence: true
   belongs_to :album
   belongs_to :artist
-  scope :search_by_artist, -> (artist){ joins(:artist).where('artists.name COLLATE utf8_general_ci LIKE ?', artist.strip).includes(:artist, :album) }
-  scope :search_by_album, -> (album) { joins(:album).where('albums.name COLLATE utf8_general_ci LIKE ?', album.strip).includes(:artist, :album)}
-  scope :search_by_song, -> (song) { where('name COLLATE utf8_general_ci LIKE ?', song.strip).includes(:artist, :album) }
-  scope :search_by_artist_album, -> (artist, album) { joins(:album).where('albums.name COLLATE utf8_general_ci LIKE ?', album.strip).joins(:artist).where('artists.name LIKE ?',artist.strip).includes(:artist, :album)}
-  scope :search_by_artist_song, -> (artist, song) { where('songs.name COLLATE utf8_general_ci LIKE ?', song.strip).joins(:artist).where('artists.name COLLATE utf8_general_ci LIKE ?', artist.strip).includes(:artist, :album)}
-  scope :search_by_album_song, -> (album, song) { where('songs.name COLLATE utf8_general_ci LIKE ?', song.strip).joins(:album).where('albums.name COLLATE utf8_general_ci LIKE ?', album.strip).includes(:artist, :album)}
-  scope :search_by_all, -> (artist, album, song) { where('songs.name COLLATE utf8_general_ci LIKE ?', song.strip).joins(:artist).where('artists.name COLLATE utf8_general_ci LIKE ?', artist.strip).joins(:album).where('albums.name COLLATE utf8_general_ci LIKE ?', album.strip).includes(:album, :artist)}
+  scope :search_by_artist, -> (artist){ joins(:artist).where('artists.name LIKE ? ', '%' + artist.strip + '%').includes(:artist, :album) }
+  scope :search_by_album, -> (album) { joins(:album).where('albums.name LIKE ?','%' + album.strip + '%').includes(:artist, :album)}
+  scope :search_by_song, -> (song) { where('name  LIKE ?', '%' + song.strip + '%').includes(:artist, :album) }
+  scope :search_by_artist_album, -> (artist, album) { joins(:album).where('albums.name  LIKE ?', '%' + album.strip + '%').joins(:artist).where('artists.name LIKE ?','%' + artist.strip + '%').includes(:artist, :album)}
+  scope :search_by_artist_song, -> (artist, song) { where('songs.name  LIKE ?','%' + song.strip + '%').joins(:artist).where('artists.name  LIKE ?','%' + artist.strip + '%').includes(:artist, :album)}
+  scope :search_by_album_song, -> (album, song) { where('songs.name  LIKE ?', '%' +song.strip + '%').joins(:album).where('albums.name  LIKE ?', '%' + album.strip + '%').includes(:artist, :album)}
+  scope :search_by_all, -> (artist, album, song) { where('songs.name  LIKE ?','%' + song.strip + '%').joins(:artist).where('artists.name  LIKE ?','%' + artist.strip + '%').joins(:album).where('albums.name LIKE ?','%' + album.strip + '%').includes(:album, :artist)}
 end
